c5d07d62e1666596c3dabca4b140e4840031c902
Zain Saqib <135007714+Xain1501@users.noreply.github.com>
2025-05-11 13:11:00 +0500

    Update QM.py
    added comments on certain features and functionalities to may be build if i get time to enhance the project

--- QM.py
+++ QM.py
@@ -53,3 +53,13 @@ for index in range(StudNum):
     print(name + " scored " + str(score) + "/" + str(numQ))
 
 print("\nThank you for using Zain Quizzes!\n")
+
+
+#features that can be added to improve the project
+# instead of telling the answers right away give em a report of what they did correct and what they did wrong
+# ask them if they would like multiple choice questions or like normal question answers 
+# make  a standard use percentages and grades add formatting like dashes to separate sections figure out a ways to like make an admin screen and student screen so the student just cant scroll up and answer the stuff from there ony 
+# make sure ppl make questions in a format to help grading like total questions can be like a multiple of 5s but still less than a hundred questions
+# add an option for children to skip the question or move fwd and backward to and fro so they can jump to some questions 
+# tell them which questions would they like to attempt every turn they get 
+# can also add an option to time the children and end the quiz if the time limit exceeds find out the current time and tell them what time its is and how much time they got
